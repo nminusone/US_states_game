aeb082cb3699d2e6826da5d034e15081bc7441a9
nminusone <obmar2055@yahoo.com>
2021-12-08 13:01:28 -0500

    Final

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from turtle import Turtle, Screen
+
+import pandas as pd
+
 from guess import Guessing
 from draw import Draw
 
@@ -17,13 +20,14 @@ screen.addshape(image)
 # instead of drawing 'turtle' or 'circle' etc., draws the image defined
 background_map.shape(image)
 # Window pops up and asks for state
-still_playing = True
 
-while still_playing:
-    answer_state.guess_to_check(screen.textinput(title='Guess the State', prompt="Type the name of a state!"))
+while len(answer_state.guess_list) < 50:
+    answer_state.guess_to_check(screen.textinput(title=f'{len(answer_state.guess_list)}/50 States Correct', prompt=
+    "Type the name of a state!"))
+    if answer_state.user_guess == 'Exit':
+        break
     if answer_state.check_list():
         sam.write_state(int(answer_state.x), int(answer_state.y), answer_state.user_guess)
-        print('Hello')
 
 # This is how csv values were determined
 # def get_mouse_click_coor(x, y):
@@ -32,5 +36,8 @@ while still_playing:
 #
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
-
-screen.exitonclick()
+if len(answer_state.guess_list) < 50:
+    states_to_learn = list(set(answer_state.states) - set(answer_state.guess_list))
+    to_study= pd.DataFrame(states_to_learn, columns=['States you missed'])
+    to_study.to_csv('Study_this.csv')
+    print(states_to_learn)
